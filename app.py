9bc8384551c6bd30f7f99c5db06c10060af8a1c2
adgsenpai <adgrules@hotmail.com>
2022-04-15 23:53:01 +0200

    Change of Framework using - React Update

--- app.py
+++ app.py
@@ -3,8 +3,9 @@
 from flask import Flask,render_template,send_from_directory,jsonify,request
 import WebComponents
 import FetchData
+from flask_cors import CORS
 app = Flask(__name__,template_folder='./pages')
-
+CORS(app)
 # allows for files to be refreshed in server
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
@@ -19,21 +20,23 @@ def home():
 def returnRegData():
   return jsonify(RegData)
 
-@app.route('/api/v1/getGeneralInformation',methods=['POST'])
+@app.route('/api/v1/getData',methods=['POST'])
 def getGeneralInformation():
   if request.data:
      data = request.get_json()
-     print(data)
-     return {'result':FetchData.get_info(data['city'])}
+     try:
+      imageURL = FetchData.get_image(data['city'])
+     except:
+      imageURL = 'none'
+     try:
+      generalInfo = FetchData.get_info(data['city'])
+     except:
+      generalInfo = 'none'
+     return {'imageURL':imageURL,'generalInfo':generalInfo}
   else:
       return {"error":"invalid syntax for API"}
   
-@app.route('/api/v1/renderImages',methods=['POST'])
-def renderImages():
-  if request.data:
-      data = request.get_json()
-      return {'result':FetchData.get_image(data['city'])}
-  return {"error":"invalid syntax for API"}
+ 
 
 #running server on port 5000 - you can change the values here
 if __name__ == "__main__":
